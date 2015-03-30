9be09df6ff6ec9b4733176a5f6e234e273a39f8a
교희 <@youngyohee>
2015-03-30 16:50:05 +0900

    DAO refactoring

---
+++ 성적처리1/src/data_access_object/DAO.java
@@ -0,0 +1,41 @@
+package data_access_object;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class DAO {
+	public Object read(String fileName)
+	{
+		Object object = null;
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName));
+			object = in.readObject();
+			in.close();
+			
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return object;
+	}
+	
+	public static void write(Object object, String fileName) {
+		// TODO Auto-generated method stub
+		
+		//과목 write
+		//object output stream
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName));
+			out.writeObject(object);
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // exception control한다고 함
+	}
+
+}
